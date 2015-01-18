445c18fe11c58a0a17edc05aa9a5e4381f442866
andy fang <andy@mingtak.com.tw>
2015-01-18 12:22:24 +0000

    add viewlets

---
+++ twtour/content/viewlets/viewlets.py
@@ -0,0 +1,47 @@
+#-*- coding:utf-8 -*-
+from zope.interface import Interface
+from five import grok
+from plone.app.layout.viewlets.interfaces import IPortalHeader, IAboveContent, IBelowContentBody
+#from reporter.content.eventinfo import IEventInfo
+#from reporter.content.article import IArticle
+#from reporter.content.contentlist import IContentList
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+"""
+# viewlet nameing format: ContentType_Position_ViewletFunction
+class EventInfo_IAboveContent_ResponsiveAd(grok.Viewlet):
+    grok.viewletmanager(IAboveContent)
+    grok.context(IEventInfo)
+    template = ViewPageTemplateFile('template/responsivead.pt')
+
+    def render(self):
+        return self.template()
+
+
+class All_IBelowContentBody_TopBanner(grok.Viewlet):
+    grok.viewletmanager(IBelowContentBody)
+    grok.context(Interface)
+    template = ViewPageTemplateFile('template/topbanner.pt')
+
+    def render(self):
+        return self.template()
+
+
+class All_IBelowContentBody_Sidebar(grok.Viewlet):
+    grok.viewletmanager(IBelowContentBody)
+    grok.context(Interface)
+    template = ViewPageTemplateFile('template/sidebar.pt')
+
+    def render(self):
+        return self.template()
+
+
+class All_IBelowContentBody_WebHeaderTop(grok.Viewlet):
+    grok.viewletmanager(IBelowContentBody)
+    grok.context(Interface)
+    template = ViewPageTemplateFile('template/webheadertop.pt')
+
+    def render(self):
+        return self.template()
+"""
